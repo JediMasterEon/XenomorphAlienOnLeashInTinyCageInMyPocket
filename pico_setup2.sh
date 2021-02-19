25adc90d24b1028cffb0bee2e07311eb1c581ef1
Sean Reichle <JediMasterEon@email.com>
2021-02-19 05:33:37 -0800

    more

---
+++ pico_setup2.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/JediMasterEon/pico-project-generator.git
+git clone https://github.com/JediMasterEon/micropython.git
+git clone https://github.com/JediMasterEon/micropython-lib.git
+git clone https://github.com/JediMasterEon/Adafruit_CircuitPython_CircuitPlayground.git
